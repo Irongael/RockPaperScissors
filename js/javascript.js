59dd31c1e69f9fad5faaf6f60a99bba8bfdc1f8c
irongael <126038141+Irongael@user.noreply.github.com>
2023-04-11 00:08:04 -0400

    finish basic 5 round game logic

--- js/javascript.js
+++ js/javascript.js
@@ -17,6 +17,7 @@ function getComputerChoice()
 }
 
 function decodeChoice(number) {
+    //decodes what the random compchoice is "rock paper or scissors"
     if(number == 0) 
     {
         console.log("Computer Player Chooses Rock!");
@@ -35,54 +36,101 @@ function decodeChoice(number) {
     }
 }
 
-let compSel = getComputerChoice();
-
-let playSel = prompt("Select Rock Paper or Scissors to play!", "Rock");
-
 function playRound(playerSelection, compSelection)
 {
+    //plays a round of rock paper scissors and checks which player wins.
     if(compSelection == 0 && (playerSelection.toUpperCase()) == "SCISSORS") 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
         console.log("You Lose!");
+        return 0; //returns 0 for a loss
     }
     else if(compSelection == 0 && (playerSelection.toUpperCase()) == "PAPER") 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
         console.log("You Win!");
+        return 1; //returns 1 for a win
     }
     else if(compSelection == 1 && (playerSelection.toUpperCase()) == "ROCK") 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
         console.log("You Lose!");
+        return 0;
     }
     else if(compSelection == 1 && (playerSelection.toUpperCase()) == "SCISSORS") 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
         console.log("You Win!");
+        return 1;
     }
     else if(compSelection == 2 && (playerSelection.toUpperCase()) == "PAPER") 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
         console.log("You Lose!");
+        return 0;
     }
     else if(compSelection == 2 && (playerSelection.toUpperCase()) == "ROCK") 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
         console.log("You Win!");
+        return 1;
     }
     else 
     {
         decodeChoice(compSelection);
         console.log("You Picked " + playerSelection);
-        console.log("No Winner Play Again!")
+        return 2; //returns 2 for inconclusive
     }
 }
 
-playRound(playSel, compSel);
+function game() 
+{
+    let playScore = 0;
+    let compScore = 0;
+    let gameCount = 0;
+
+    while(gameCount < 5 && compScore < 3 && playScore < 3)
+    {
+        let compSel = getComputerChoice();
+        let playSel = prompt("Select Rock Paper or Scissors to play!", "Enter Choice");
+        let result = playRound(playSel, compSel);
+
+        if(result == 0)
+        {
+            compScore++;
+            gameCount++;
+        }
+        else if(result == 1)
+        {
+            playScore++;
+            gameCount++;
+        }
+        else
+        {
+            console.log("No Winner Play Again!")
+        }
+        console.log("Player Score: " + playScore);
+        console.log("Computer Score: " + compScore);
+    }
+
+    if(playScore >= 3)
+    {
+        console.log("Player Wins Best of 5!")
+    }
+    else if(compScore >= 3)
+    {
+        console.log("Computer Wins Best of 5! Better Luck Next Time!")
+    }
+    else 
+    {
+        console.log("nobody won interesting...")
+    }
+}
+
+game();
