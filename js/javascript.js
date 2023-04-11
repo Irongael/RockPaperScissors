64ca02bc0ab86d5812ad305ef01016ebb083c96e
irongael <126038141+Irongael@user.noreply.github.com>
2023-04-10 23:18:24 -0400

    minor change to javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,12 @@
-function getComputerChoice() { //uses Math.random to assign a choice for the computer player
+function getComputerChoice() 
+{ //uses Math.random to assign a choice for the computer player
     let compNum = Math.random();
 
-    if(compNum <= 0.3)
+    if(compNum <= 0.3332)
     {
         return 0;
     }
-    else if(compNum > 0.3 && compNum < 0.7)
+    else if(compNum >= 0.3333 && compNum < 0.6667)
     {
         return 1;
     }
@@ -16,13 +17,16 @@ function getComputerChoice() { //uses Math.random to assign a choice for the com
 }
 
 function decodeChoice(number) {
-    if(number == 0) {
+    if(number == 0) 
+    {
         console.log("Computer Player Chooses Rock!");
     }
-    else if(number == 1) {
+    else if(number == 1) 
+    {
         console.log("Computer Player Chooses Paper!");
     }
-    else if(number == 2) {
+    else if(number == 2) 
+    {
         console.log("Computer Player Chooses Scissors!");
     }
     else 
@@ -30,3 +34,14 @@ function decodeChoice(number) {
         console.log("Computer Player Chooses a Gun?! Something is Wrong.");
     }
 }
+
+let compSel = getComputerChoice();
+
+let playSel = prompt("Select Rock Paper or Scissors to play!", "Rock");
+
+function playRound(playerSelection, compSelection)
+{
+
+}
+
+decodeChoice(compSel);
